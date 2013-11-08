97b890d58b97f29ad2aa8a0e4ad1f888442d0cc6
Pranav Singh <pranavkrs1983@yahoo.com>
2013-11-07 17:37:07 -0800

    c2

---
+++ src/java/lab/cmpe273/flight/admin/model/Person.java
@@ -0,0 +1,58 @@
+package java.lab.cmpe273.flight.admin.model;
+
+public class Person {
+	
+	public Person() {}
+	
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	String zipcode;
+	String dob;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+}
