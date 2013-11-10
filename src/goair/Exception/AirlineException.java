f1efd7f44006a4c15fa441920a8d5e9af6846b78
Amrita Mohanty <amrita_mohanty2004@yahoo.com>
2013-11-10 15:07:12 -0800

    Adding comments.

--- src/goair/Exception/AirlineException.java
+++ src/goair/Exception/AirlineException.java
@@ -1,5 +1,8 @@
 package goair.Exception;
 
+/**
+ * This class holds all the possible exceptions that can be thrown by this project.
+ */
 public class AirlineException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
