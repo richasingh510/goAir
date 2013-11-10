fd5ba841da540e93365d76a6383f3f151a2c52d9
Amrita Mohanty <amritamohanty2004@gmail.com>
2013-11-10 00:44:47 -0800

    -

---
+++ src/goair/booking/BookingBean.java
@@ -0,0 +1,39 @@
+package goair.booking;
+
+import goair.customer.Customer;
+import goair.flight.Flight;
+
+public class BookingBean {
+	public Customer customerBean;
+	public Flight flightBean;
+	public int reservationId;
+	
+	public Customer getCustomerBean() {
+		return customerBean;
+	}
+	public void setCustomerBean(Customer customerBean) {
+		this.customerBean = customerBean;
+	}
+	public Flight getFlightBean() {
+		return flightBean;
+	}
+	public void setFlightBean(Flight flightBean) {
+		this.flightBean = flightBean;
+	}
+	public int getReservationId() {
+		return reservationId;
+	}
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+	
+	@Override
+	public String toString() {
+		return "BookingBean [customerBean=" + customerBean + ", flightBean="
+				+ flightBean + ", reservationId=" + reservationId + "]";
+	}
+	
+	
+	
+}
+
