8a8269e0387bdf40ffc2cf9fe225de3739fceb16
Richa Singh <richa.singh510@gmail.com>
2013-11-09 21:30:15 -0800

    c6

--- src/goair/customer/Customer.java
+++ src/goair/customer/Customer.java
@@ -1,7 +1,32 @@
 package goair.customer;
+import goair.general.Person;
 
-public class Customer {
+public class Customer extends Person{
+
+	public Customer() {}
+
+	String customerId;
+	int passportNo;
+	String nationality;
 	
-	// 
-	
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public int getPassportNo() {
+		return passportNo;
+	}
+	public void setPassportNo(int passportNo) {
+		this.passportNo = passportNo;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
 }
+
--- src/goair/employee/Employee.java
+++ src/goair/employee/Employee.java
@@ -3,5 +3,37 @@ package goair.employee;
 import goair.general.Person;
 
 public class Employee extends Person {
+	public Employee() {}
+
+	String empId;
+	String empDes;
+	String empPosition;
+	String empHiredate;
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpDes() {
+		return empDes;
+	}
+	public void setEmpDes(String empDes) {
+		this.empDes = empDes;
+	}
+	public String getEmpPosition() {
+		return empPosition;
+	}
+	public void setEmpPosition(String empPosition) {
+		this.empPosition = empPosition;
+	}
+	public String getEmpHiredate() {
+		return empHiredate;
+	}
+	public void setEmpHiredate(String empHiredate) {
+		this.empHiredate = empHiredate;
+	}
 
 }
+
