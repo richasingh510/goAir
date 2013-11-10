ec5f7c68eeeb76db77043997fdc9a7ec1f8d6cfb
Amrita Mohanty <amritamohanty2004@gmail.com>
2013-11-09 19:42:21 -0800

    Adding functions that admin can perform.

--- src/goair/wsdl/AdminServices.java
+++ src/goair/wsdl/AdminServices.java
@@ -1,5 +1,12 @@
 package goair.wsdl;
 
 public class AdminServices {
-
+	
+	//When you login as admin you see following hyper links
+	// 1. Search flight - when you get a searched flight you get option to edit / delete it.
+	// 2. View all flights - when you view a flight you get option to edit / delete it.
+	// 3. Add a flight
+	
+	// View flight page will have flight details with availability.
+	
 }
