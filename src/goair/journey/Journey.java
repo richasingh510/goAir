4483db78a7d0912b4a6f82deabff43cb63428289
Pranav Singh <pranav.kr.singh@gmail.com>
2013-11-09 20:45:16 -0800

    journey

---
+++ src/goair/journey/Journey.java
@@ -0,0 +1,30 @@
+package goair.journey;
+
+public class Journey {
+	
+	public Journey() {}
+	
+	String flightNumber;
+	String destination;
+	String boardingPoint;
+	
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getBoardingPoint() {
+		return boardingPoint;
+	}
+	public void setBoardingPoint(String boardingPoint) {
+		this.boardingPoint = boardingPoint;
+	}
+	
+}
