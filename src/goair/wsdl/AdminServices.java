0075354af227a2a827fcd0d66ff241244011d62c
Amrita Mohanty <amritamohanty2004@gmail.com>
2013-11-09 19:47:44 -0800

    Some more functions added for admin.

--- src/goair/wsdl/AdminServices.java
+++ src/goair/wsdl/AdminServices.java
@@ -7,6 +7,16 @@ public class AdminServices {
 	// 2. View all flights - when you view a flight you get option to edit / delete it.
 	// 3. Add a flight
 	
+	// 4. Search customer - when you get a searched customer you get option to edit / delete it.
+	// 5. View all customers - when you view a customer you get option to edit / delete it.
+	// 6. Add a customer 
+	
+	// 4. Search employee - when you get a searched employee you get option to edit / delete it.
+	// 5. View all employee - when you view a employee you get option to edit / delete it.
+	// 6. Add a employee 
+	
 	// View flight page will have flight details with availability.
+	// View customer page will have customer details with booking history and all.
+	// View employee page will have employee details with profile and all.
 	
 }
